ccf45bf7a1a88085042903387f7791a0c681249e
PolinaKuznecova87 <poly.voinylenko@gmail.com>
2022-11-27 19:04:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int tickets = 2000;
+        //Стоимость билета
+        int bonusMili = tickets / 20;
+
+
+        System.out.println("Стоимость билета " + tickets);
+        System.out.println("Бонусные мили  " + bonusMili);
+
+    }
+}
